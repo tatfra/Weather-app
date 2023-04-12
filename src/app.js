4c086c80543324468a378aba1a22514c0fd90041
tatfra <fraga.ribeiro.tatiana@gmail.com>
2023-04-12 23:24:02 +0100

    Update html structure

--- src/app.js
+++ src/app.js
@@ -1,34 +1,53 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+let now = new Date();
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "Mars",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = "0" + hour;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = "0" + minutes;
 }
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let date = now.getDate();
+console.log(day);
+console.log(hour);
+console.log(minutes);
+console.log(month);
 
-  return days[day];
-}
+let year = now.getFullYear();
+
+let fullDate = document.querySelector("#date");
+fullDate.innerHTML = `${year} ${month}, ${date}`;
+
+let dayTime = document.querySelector("#day");
+dayTime.innerHTML = `${day}, ${hour}:${minutes}`;
 
 function getForecast(coordinates) {
   let apikey = "21eea3638bfada0d7c0b605eac8f5457";
@@ -45,22 +64,29 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-
+  let sunriseElement = document.querySelector("#sunrise");
+  let sunsetElement = document.querySelector("#sunset");
   celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  sunriseElement.innerHTML = Math.round(
+    response.data.sys.sunrise * 1000
+  ).format("HH:mm a");
+  sunsetElement.innerHTML = Math.round(response.data.sys.sunset * 1000).format(
+    "HH:mm a"
+  );
   humidityElement.innerHTML = response.data.main.humidity;
-  //windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
-  //dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  //iconElement.setAttribute(
-  // "src",
-  //`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  //);
-  //iconElement.setAttribute("alt", response.data.weather[0].description);
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  //getForecast(response.data.coord);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -78,4 +104,4 @@ function handleSubmit(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-search("New York");
+search("Porto");
